f975e8a088e5924bdee0dca810418f397e55a74b
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2021-12-20 21:34:36 +0100

    ajout fonctions de calcul et vérification de checksum

--- src/www/protected/fonctions.php
+++ src/www/protected/fonctions.php
@@ -26,3 +26,35 @@ function addlog($msg)
         fclose($f);
     }
 }
+
+// Fonctions de calcul de checksum et de contrôle du résultat
+// https://trucs-de-developpeur-web.fr/p/_3002-calculer-et-verifier-un-checksum-pour-dialoguer-avec-l-exterieur.html
+
+// return a checksum value for "verif" URL param
+function getVerifChecksum($param, $key1 = "", $key2 = "", $key3 = "", $key4 = "", $key5 = "", $public = true)
+{
+    $verif = "";
+    if (is_array($param)) {
+        $par = "";
+        foreach ($param as $value) {
+            $par .= $value;
+        }
+        $verif = md5($par . $key1 . $key2 . $key3 . $key4 . $key5);
+    } else {
+        $verif = md5($param . $key1 . $key2 . $key3 . $key4 . $key5);
+    }
+    return ($public) ? substr($verif, mt_rand(0, strlen($verif) - 10), 10) : $verif;
+}
+
+// check a "verif" checksum value
+// return TRUE if ok, FALSE if not
+function checkVerifChecksum($verif, $param, $key1 = "", $key2 = "", $key3 = "", $key4 = "", $key5 = "")
+{
+    if ((strlen($verif) < 1) && isset($_POST["verif"])) {
+        $verif = $_POST["verif"];
+    }
+    if ((strlen($verif) < 1) && isset($_GET["verif"])) {
+        $verif = $_GET["verif"];
+    }
+    return (false !== strpos(getVerifChecksum($param, $key1, $key2, $key3, $key4, $key5, false), $verif));
+}
