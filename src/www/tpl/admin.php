501582cb744ae58c3eea054dc3b8bbeebb5a1797
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2021-12-22 21:01:19 +0100

    ajout de la gestion des types de contrats

--- src/www/tpl/admin.php
+++ src/www/tpl/admin.php
@@ -20,6 +20,7 @@ require_once(__DIR__ . "/_header.php");
         <li><a href="admin-pays.php">Gestion des pays</a></li>
         <li><a href="admin-reseaux-sociaux.php">Gestion des réseaux sociaux</a></li>
         <li><a href="admin-rubriquescv.php">Gestion des rubriques sur les CV</a></li>
+        <li><a href="admin-types-contrats.php">Gestion des types de contrats</a></li>
     </ul><?php
 require_once(__DIR__ . "/_footer.php");
 
