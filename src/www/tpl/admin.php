47be325a3ae2a51ae4d802b81cac4d19240aedf9
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2021-12-22 20:51:46 +0100

    ajout écran de gestion des réseaux sociaux

--- src/www/tpl/admin.php
+++ src/www/tpl/admin.php
@@ -18,6 +18,7 @@ require_once(__DIR__ . "/_header.php");
         <li><a href="admin-langues.php">Gestion des langues</a></li>
         <li><a href="admin-niveaux-etudes.php">Gestion des niveaux d'études</a></li>
         <li><a href="admin-pays.php">Gestion des pays</a></li>
+        <li><a href="admin-reseaux-sociaux.php">Gestion des réseaux sociaux</a></li>
     </ul><?php
 require_once(__DIR__ . "/_footer.php");
 
