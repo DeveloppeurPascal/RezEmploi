b143bcb5ee23c3b2f0d9dfb53d8af55c9db68c96
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2021-12-24 21:52:23 +0100

    remplissage de la page "index membre" à partir des textes saisis en base de données

--- src/www/tpl/index-membre.php
+++ src/www/tpl/index-membre.php
@@ -12,7 +12,27 @@ if (!isUtilisateurConnecte()) {
 
 $PageTitle = _TitreDuSite;
 require_once(__DIR__ . "/_header.php");
-?><?php
+require_once(__DIR__ . "/../" . ProtectedFolder . "/textesdusite.php"); ?>
+    <section class="fdb-block py-0">
+    <div class="container py-5 my-5" style="background-image: url(imgs/shapes/9.svg);">
+        <div class="row py-5">
+            <div class="col py-5">
+                <div class="fdb-box fdb-touch">
+                    <div class="row text-center justify-content-center">
+                        <div class="col-12 col-md-9 col-lg-7"><?php
+                            if (getTexteDuSite("index-membre", $titre, $texte)) {
+                                if (!empty($titre)) print("<h1>" . $titre . "</h1>");
+                                print($texte);
+                            } else {
+                                require_once(__DIR__ . "/../" . ProtectedFolder . "/erreurs.php");
+                                ajoute_erreur("Page \"index-membre\" non trouvée.");
+                            } ?></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div></section><?php
+require_once(__DIR__ . "/_erreur-bloc-html.php");
 require_once(__DIR__ . "/_footer.php");
 
 // TODO : rappel infos de l'utilisateur
