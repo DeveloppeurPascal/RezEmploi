f814e965430107b47396602242c686bff2037761
Patrick Prémartin <patrick.premartin@developpeur-pascal.fr>
2021-12-29 20:21:29 +0100

    ajout <FORM> au HTML de la page de contact, à compléter ultérieurement

--- src/www/tpl/contact.php
+++ src/www/tpl/contact.php
@@ -8,26 +8,27 @@ $PageTitle = "Contacter " . _TitreDuSite;
 require_once(__DIR__ . "/_header.php");
 require_once(__DIR__ . "/../" . ProtectedFolder . "/textesdusite.php");
 ?>
+    <form action="contact.php" method="post" id="frm">
+    <input type="hidden" name="op" id="op" value="contact">
     <section class="fdb-block py-0">
-    <div class="container py-5 my-5" style="background-image: url(imgs/shapes/9.svg);">
-        <div class="row py-5">
-            <div class="col py-5">
-                <div class="fdb-box fdb-touch">
-                    <div class="row text-center justify-content-center">
-                        <div class="col-12 col-md-9 col-lg-7"><?php
-                            if (getTexteDuSite("contact-haut", $titre, $texte)) {
-                                if (!empty($titre)) print("<h1>" . $titre . "</h1>");
-                                print($texte);
-                            } else {
-                                require_once(__DIR__ . "/../" . ProtectedFolder . "/erreurs.php");
-                                ajoute_erreur("Page \"contact-haut\" non trouvée.");
-                            } ?>
+        <div class="container py-5 my-5" style="background-image: url(imgs/shapes/9.svg);">
+            <div class="row py-5">
+                <div class="col py-5">
+                    <div class="fdb-box fdb-touch">
+                        <div class="row text-center justify-content-center">
+                            <div class="col-12 col-md-9 col-lg-7"><?php
+                                if (getTexteDuSite("contact-haut", $titre, $texte)) {
+                                    if (!empty($titre)) print("<h1>" . $titre . "</h1>");
+                                    print($texte);
+                                } else {
+                                    require_once(__DIR__ . "/../" . ProtectedFolder . "/erreurs.php");
+                                    ajoute_erreur("Page \"contact-haut\" non trouvée.");
+                                } ?>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row justify-content-center pt-4">
-                        <div class="col-12 col-md-8">
-                            <form>
+                        <div class="row justify-content-center pt-4">
+                            <div class="col-12 col-md-8">
                                 <div class="row">
                                     <div class="col-12 col-md">
                                         <input type="text" class="form-control" placeholder="Name">
@@ -60,14 +61,13 @@ require_once(__DIR__ . "/../" . ProtectedFolder . "/textesdusite.php");
                                     require_once(__DIR__ . "/../" . ProtectedFolder . "/erreurs.php");
                                     ajoute_erreur("Page \"contact-bas\" non trouvée.");
                                 } ?>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    </section><?php
+    </section></form><?php
 require_once(__DIR__ . "/_erreur-bloc-html.php");
 require_once(__DIR__ . "/_footer.php");
 
